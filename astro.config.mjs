8dcdd47906b94f5b42137297463795563496f351
jjenk <j.jenkinson89@hotmail.com>
2025-10-17 18:10:27 +0200

    - fixed link colour on light mode in projects popup
    - commented out form and added temp msg
    
    - TODO: rework form for Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
-import vercel from "@astrojs/vercel";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: cloudflare(),
-    integrations: [sitemap()],
+	adapter: cloudflare(),
+	integrations: [sitemap()],
 });
